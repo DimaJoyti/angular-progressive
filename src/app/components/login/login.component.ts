e637396cc86170c2860d3e8a2f1a5fcb2d5ec447
DimaGlushchenko <aws.inspiration@gmail.com>
2018-01-06 16:41:22 +0200

    ready

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "angularfire2/auth";
+import * as firebase from "firebase/app";
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"]
 })
 export class LoginComponent implements OnInit {
+  constructor(public afAuth: AngularFireAuth) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   }
-
+  
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+  
 }
